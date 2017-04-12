17e438c23b05c16541a8191686fc17d4b8161fff
Daniel Pejcinovski <danielpejcinovski@hotmail.com>
2017-04-12 17:10:25 +0200

    Setup of window in  Openglrenderer().

--- src/OpenGL/OpenGLRenderer.cpp
+++ src/OpenGL/OpenGLRenderer.cpp
@@ -1,7 +1,14 @@
 #include "OpenGLRenderer.hpp"
+#include <gl/glew.h>
 
-OpenGLRenderer::OpenGLRenderer() {
-    
+OpenGLRenderer::OpenGLRenderer(Window window) {
+    // Setup glfw window.
+    window.setWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    window.setWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    window.setWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    window.setWindowHint(GLFW_RESIZABLE, GL_FALSE);
+    std::string windowTitle = "OpenGL";
+    window.createWindow(windowTitle);
 }
 
 OpenGLRenderer::~OpenGLRenderer() {
