aa810956f4f8442f1583eb47362846243881d9d8
Daniel <danielpejcinovski@hotmail.com>
2017-04-12 16:35:27 +0200

    Added static window and undid woopsie.

--- src/Base/Renderer.cpp
+++ src/Base/Renderer.cpp
@@ -2,17 +2,20 @@
 #include "../OpenGL/OpenGLRenderer.hpp"
 #include "../Vulkan/VulkanRenderer.hpp"
 
+Window* Renderer::window = nullptr;
+
 Renderer::~Renderer() {
     
 }
 
-Renderer* Renderer::makeRenderer(BACKEND backend){
+Renderer* Renderer::makeRenderer(BACKEND backend, Window & createdWindow){
+    window = &createdWindow;
     // Create and return backend.
     if(backend == BACKEND::OpenGL)
-        return new VulkanRenderer();
+        return new OpenGLRenderer();
     
     else if(backend == BACKEND::Vulkan)
-        return new OpenGLRenderer();
+        return new VulkanRenderer();
     
     return nullptr;
 }
