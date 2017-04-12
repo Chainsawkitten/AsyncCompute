8635ba52ee787c276b2017bcd5bd05155c1fc3e8
Daniel <danielpejcinovski@hotmail.com>
2017-04-12 15:46:17 +0200

    Window setup.

--- src/Base/Window.hpp
+++ src/Base/Window.hpp
@@ -1,7 +1,25 @@
 #pragma once
 #include <GLFW/glfw3.h>
+#include <string>
 
 class Window {
+    public:
+        /// setWindowHint Sets a GLFW window hint. Has to be called before creating window.
+        /**
+         * @param field The field that will be set.
+         * @param value The value to set.
+         */
+        void setWindowHint(int field, int value);
 
+        /// createWindow Creates a GLFW window.
+        /**
+         * @param windowTitle The title of the glfw window.
+         * @return Created GLFW window.
+         */
+        GLFWwindow* createWindow(std::string windowTitle);
 
+    private:
+        GLFWwindow* window;
+        int width = 800;
+        int height = 600;
 };
