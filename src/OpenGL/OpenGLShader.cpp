fdf8dfc21fb9d954c86d69f8339e44aba216b77f
Daniel Pejcinovski <danielpejcinovski@hotmail.com>
2017-04-18 18:00:53 +0200

    Started work on OpenGLShader.

--- src/OpenGL/OpenGLShader.cpp
+++ src/OpenGL/OpenGLShader.cpp
@@ -1,5 +1,25 @@
 #include "OpenGLShader.hpp"
+#include "default.vert.hpp"
 
 OpenGLShader::OpenGLShader(){
+    GLchar infoLog[512];
+    GLint success;
+    vertexShaderHandle = glCreateShader(GL_VERTEX_SHADER);
 
+    GLint length = DEFAULT_VERT_LENGTH;
+    const GLchar* source = DEFAULT_VERT;
+
+    // Set shader source.
+    glShaderSource(vertexShaderHandle, 1, &source, &length);
+
+    // Compile shader.
+    glCompileShader(vertexShaderHandle);
+
+    glGetShaderiv(vertexShaderHandle, GL_COMPILE_STATUS, &success);
+
+    // Get compile output.
+    if(!success){
+        glGetShaderInfoLog(vertexShaderHandle, 512, nullptr, infoLog);
+
+    }
 }
--- src/OpenGL/OpenGLShader.hpp
+++ src/OpenGL/OpenGLShader.hpp
@@ -1,7 +1,10 @@
 #pragma once
+#include <GL/glew.h>
 
 class OpenGLShader {
     public:
+        // Constructor
         OpenGLShader();
     private:
+        GLuint vertexShaderHandle;
 };
