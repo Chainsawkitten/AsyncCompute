8de86f42ef2dd01212fd76f043e07b9d0e5b19c5
Albin Bernhardsson <catscratchesp@gmail.com>
2017-04-19 13:43:22 +0200

    Load texture from memory

--- src/Vulkan/VulkanTexture.cpp
+++ src/Vulkan/VulkanTexture.cpp
@@ -1,7 +1,20 @@
 #include "VulkanTexture.hpp"
 
+#define STB_IMAGE_IMPLEMENTATION
+#include <stb_image.h>
+#include <iostream>
+
 VulkanTexture::VulkanTexture(const char* data, unsigned int length) {
+    // Load texture from memory.
+    int width, height, channels;
+    stbi_uc* pixels = stbi_load_from_memory(reinterpret_cast<const unsigned char*>(data), length, &width, &height, &channels, 0);
+    if (pixels == nullptr) {
+        std::cerr << "Failed to load image from memory." << std::endl;
+        exit(-1);
+    }
     
+    // Clean up.
+    stbi_image_free(pixels);
 }
 
 VulkanTexture::~VulkanTexture() {
