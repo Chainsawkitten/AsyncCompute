45ac514fbf354c33548997728b656bd4595eecf1
Daniel Pejcinovski <danielpejcinovski@hotmail.com>
2017-04-19 13:59:31 +0200

    Set clear color to black.

--- src/OpenGL/OpenGLRenderer.cpp
+++ src/OpenGL/OpenGLRenderer.cpp
@@ -40,6 +40,6 @@ void OpenGLRenderer::setTexture(const char* textureData, unsigned int dataLength
 }
 
 void OpenGLRenderer::render(){
-    glClearColor(1.0f, 0.0f, 0.0f, 1.0f);
+    glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
     glClear(GL_COLOR_BUFFER_BIT);
 }
