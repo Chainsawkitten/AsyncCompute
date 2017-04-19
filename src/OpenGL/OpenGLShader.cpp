f8e9a1c772c710e4dd237c72207edac55d8ae646
Daniel Pejcinovski <danielpejcinovski@hotmail.com>
2017-04-19 16:19:31 +0200

    Create and compile fragment shader.

--- src/OpenGL/OpenGLShader.cpp
+++ src/OpenGL/OpenGLShader.cpp
@@ -27,6 +27,43 @@ OpenGLShader::OpenGLShader(){
     // Get compile output.
     if(!success){
         glGetShaderInfoLog(vertexShaderHandle, 512, nullptr, infoLog);
-
+        std::cout << "Vertex shader compilation failed : " << infoLog << std::endl;
     }
+
+    // Fragment shader.
+    // Create fragment shader on the GPU.
+    source = DEFAULT_FRAG;
+    length = DEFAULT_FRAG_LENGTH;
+
+    fragmentShaderHandle = glCreateShader(GL_FRAGMENT_SHADER);
+
+    // Set fragment shader source.
+    glShaderSource(fragmentShaderHandle, 1, &source, &length);
+
+    // Compile fragment shader.
+    glCompileShader(fragmentShaderHandle);
+
+    glGetShaderiv(fragmentShaderHandle, GL_COMPILE_STATUS, &success);
+
+    // Get compile output.
+    if(!success){
+        glGetShaderInfoLog(fragmentShaderHandle, 512, nullptr, infoLog);
+        std::cout << "Fragment shader compilation failed : " << infoLog << std::endl;
+    }
+
+    // Create and link program.
+    shaderProgram = glCreateProgram();
+    glAttachShader(shaderProgram, vertexShaderHandle);
+    glAttachShader(shaderProgram, fragmentShaderHandle);
+    glLinkProgram(shaderProgram);
+    glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
+
+    // Get program log.
+    if(!success) {
+        glGetShaderInfoLog(shaderProgram, 512, nullptr, infoLog);
+        std::cout << "Program shader compilation failed : " << infoLog << std::endl;
+    }
+
+    glDeleteShader(vertexShaderHandle);
+    glDeleteShader(fragmentShaderHandle);
 }
