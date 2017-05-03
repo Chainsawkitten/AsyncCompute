e0c3a3d31352181d2b56cd4855c0c482965feafa
Albin Bernhardsson <catscratchesp@gmail.com>
2017-05-03 12:28:38 +0200

    Specify version 4.3 of OpenGL

--- src/OpenGL/OpenGLRenderer.cpp
+++ src/OpenGL/OpenGLRenderer.cpp
@@ -8,7 +8,7 @@
 OpenGLRenderer::OpenGLRenderer(Window & window) {
 
     // Setup glfw window.
-    window.setWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    window.setWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
     window.setWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     window.setWindowHint(GLFW_OPENGL_DEBUG_CONTEXT, GLFW_TRUE);
     window.setWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
