9f707fb9acd547cbfab860fd8b69211018268ecf
Albin Bernhardsson <catscratchesp@gmail.com>
2017-05-03 15:44:20 +0200

    Uniform buffer class

---
+++ src/UniformBuffer.cpp
@@ -0,0 +1,121 @@
+#include "UniformBuffer.hpp"
+
+#include <iostream>
+#include <cstring>
+
+UniformBuffer::UniformBuffer(const void* data, unsigned int size, VkDevice device, VkPhysicalDevice physicalDevice, VkDescriptorPool descriptorPool) {
+    this->device = device;
+    this->physicalDevice = physicalDevice;
+    this->descriptorPool = descriptorPool;
+    
+    createBuffer(size, VK_BUFFER_USAGE_UNIFORM_BUFFER_BIT, VK_MEMORY_PROPERTY_HOST_VISIBLE_BIT | VK_MEMORY_PROPERTY_HOST_COHERENT_BIT, &buffer, &bufferMemory);
+    
+    // Copy data to mapped memory.
+    void* mappedMemory;
+    vkMapMemory(device, bufferMemory, 0, size, 0, &mappedMemory);
+    memcpy(mappedMemory, data, size);
+    vkUnmapMemory(device, bufferMemory);
+    
+    // Create descriptor set.
+    createDescriptorSetLayout();
+    createDescriptorSet(size);
+}
+
+UniformBuffer::~UniformBuffer() {
+    vkDestroyDescriptorSetLayout(device, descriptorSetLayout, nullptr);
+    vkDestroyBuffer(device, buffer, nullptr);
+    vkFreeMemory(device, bufferMemory, nullptr);
+}
+
+void UniformBuffer::createBuffer(VkDeviceSize size, VkBufferUsageFlags usage, VkMemoryPropertyFlags properties, VkBuffer* buffer, VkDeviceMemory* bufferMemory) {
+    // Create buffer.
+    VkBufferCreateInfo bufferInfo = {};
+    bufferInfo.sType = VK_STRUCTURE_TYPE_BUFFER_CREATE_INFO;
+    bufferInfo.size = size;
+    bufferInfo.usage = usage;
+    bufferInfo.sharingMode = VK_SHARING_MODE_EXCLUSIVE;
+    
+    if (vkCreateBuffer(device, &bufferInfo, nullptr, buffer) != VK_SUCCESS) {
+        std::cerr << "Failed to create buffer." << std::endl;
+        exit(-1);
+    }
+    
+    // Get information about device memory.
+    VkMemoryRequirements memoryRequirements;
+    vkGetBufferMemoryRequirements(device, *buffer, &memoryRequirements);
+    
+    VkPhysicalDeviceMemoryProperties memoryProperties;
+    vkGetPhysicalDeviceMemoryProperties(physicalDevice, &memoryProperties);
+    
+    // Find suitable memory type.
+    uint32_t memoryType;
+    for (uint32_t i = 0; i < memoryProperties.memoryTypeCount; i++) {
+        if (memoryRequirements.memoryTypeBits & (1 << i) && (memoryProperties.memoryTypes[i].propertyFlags & properties) == properties )
+            memoryType = i;
+    }
+    
+    // Allocate buffer memory.
+    VkMemoryAllocateInfo allocateInfo = {};
+    allocateInfo.sType = VK_STRUCTURE_TYPE_MEMORY_ALLOCATE_INFO;
+    allocateInfo.allocationSize = memoryRequirements.size;
+    allocateInfo.memoryTypeIndex = memoryType;
+    
+    if (vkAllocateMemory(device, &allocateInfo, nullptr, bufferMemory) != VK_SUCCESS) {
+        std::cerr << "Failed to allocate buffer memory." << std::endl;
+        exit(-1);
+    }
+    
+    vkBindBufferMemory(device, *buffer, *bufferMemory, 0);
+}
+
+void UniformBuffer::createDescriptorSetLayout() {
+    VkDescriptorSetLayoutBinding vertexLayoutBinding = {};
+    vertexLayoutBinding.binding = 0;
+    vertexLayoutBinding.descriptorType = VK_DESCRIPTOR_TYPE_UNIFORM_BUFFER;
+    vertexLayoutBinding.descriptorCount = 1;
+    vertexLayoutBinding.stageFlags = VK_SHADER_STAGE_VERTEX_BIT;
+    vertexLayoutBinding.pImmutableSamplers = nullptr;
+    
+    VkDescriptorSetLayoutCreateInfo layoutInfo = {};
+    layoutInfo.sType = VK_STRUCTURE_TYPE_DESCRIPTOR_SET_LAYOUT_CREATE_INFO;
+    layoutInfo.bindingCount = 1;
+    layoutInfo.pBindings = &vertexLayoutBinding;
+    
+    if (vkCreateDescriptorSetLayout(device, &layoutInfo, nullptr, &descriptorSetLayout)) {
+        std::cerr << "Could not create descriptor set layout!" << std::endl;
+        exit(-1);
+    }
+}
+
+void UniformBuffer::createDescriptorSet(VkDeviceSize size) {
+    // Allocate descriptor set.
+    VkDescriptorSetAllocateInfo allocateInfo = {};
+    allocateInfo.sType = VK_STRUCTURE_TYPE_DESCRIPTOR_SET_ALLOCATE_INFO;
+    allocateInfo.descriptorPool = descriptorPool;
+    allocateInfo.descriptorSetCount = 1;
+    allocateInfo.pSetLayouts = &descriptorSetLayout;
+    
+    if (vkAllocateDescriptorSets(device, &allocateInfo, &descriptorSet) != VK_SUCCESS) {
+        std::cerr << "Failed to allocate descriptor set" << std::endl;
+        exit(-1);
+    }
+    
+    // Update descriptor set.
+    VkDescriptorBufferInfo bufferInfo = {};
+    bufferInfo.buffer = buffer;
+    bufferInfo.offset = 0;
+    bufferInfo.range = size;
+    
+    VkWriteDescriptorSet descriptorWrite = {};
+    descriptorWrite.sType = VK_STRUCTURE_TYPE_WRITE_DESCRIPTOR_SET;
+    descriptorWrite.dstSet = descriptorSet;
+    descriptorWrite.dstBinding = 0;
+    descriptorWrite.dstArrayElement = 0;
+    descriptorWrite.descriptorType = VK_DESCRIPTOR_TYPE_UNIFORM_BUFFER;
+    descriptorWrite.descriptorCount = 1;
+    descriptorWrite.pBufferInfo = &bufferInfo;
+    descriptorWrite.pImageInfo = nullptr;
+    descriptorWrite.pTexelBufferView = nullptr;
+    
+    vkUpdateDescriptorSets(device, 1, &descriptorWrite, 0, nullptr);
+}
---
+++ src/UniformBuffer.hpp
@@ -0,0 +1,34 @@
+#pragma once
+
+#include <vulkan/vulkan.h>
+
+/// Used for storing camera data.
+class UniformBuffer {
+    public:
+        /// Create new uniform buffer.
+        /**
+         * @param data Buffer data.
+         * @param size Buffer size.
+         * @param device Logical Vulkan device.
+         * @param physicalDevice Physical Vulkan device.
+         * @param descriptorPool Pool to allocate descriptor sets from.
+         */
+        UniformBuffer(const void* data, unsigned int size, VkDevice device, VkPhysicalDevice physicalDevice, VkDescriptorPool descriptorPool);
+        
+        /// Destructor.
+        ~UniformBuffer();
+        
+    private:
+        void createBuffer(VkDeviceSize size, VkBufferUsageFlags usage, VkMemoryPropertyFlags properties, VkBuffer* buffer, VkDeviceMemory* bufferMemory);
+        void createDescriptorSetLayout();
+        void createDescriptorSet(VkDeviceSize size);
+        
+        VkDevice device;
+        VkPhysicalDevice physicalDevice;
+        VkDescriptorPool descriptorPool;
+        
+        VkBuffer buffer;
+        VkDeviceMemory bufferMemory;
+        VkDescriptorSetLayout descriptorSetLayout;
+        VkDescriptorSet descriptorSet;
+};
