acc1f03db268aa412607807ff84c9a2f36fc1d02
Albin Bernhardsson <catscratchesp@gmail.com>
2017-05-04 12:40:46 +0200

    Start command buffer recording

--- src/Renderer.cpp
+++ src/Renderer.cpp
@@ -127,7 +127,13 @@ void Renderer::setTexture(const char* textureData, unsigned int dataLength) {
 }
 
 void Renderer::update() {
+    // Start command buffer recording.
+    VkCommandBufferBeginInfo beginInfo = {};
+    beginInfo.sType = VK_STRUCTURE_TYPE_COMMAND_BUFFER_BEGIN_INFO;
+    beginInfo.flags = VK_COMMAND_BUFFER_USAGE_ONE_TIME_SUBMIT_BIT;
+    beginInfo.pInheritanceInfo = nullptr;
     
+    vkBeginCommandBuffer(computeCommandBuffer, &beginInfo);
 }
 
 void Renderer::render() {
