896b7639bec7bc785640b2e45e632e5000588332
Albin Bernhardsson <catscratchesp@gmail.com>
2017-05-09 13:28:21 +0200

    measure command line option

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,17 @@
 #include "Renderer.hpp"
 #include "Window.hpp"
 #include <cstring>
+#include <iostream>
 #include "Particle.png.hpp"
 
 int main(int argc, char* argv[]) {
     Window window;
+    
+    bool measure = false;
+    if (argc > 1 && strcmp(argv[1], "measure") == 0) {
+        std::cout << "Measuring..." << std::endl;
+        measure = true;
+    }
     
     Renderer* renderer = new Renderer(window);
     renderer->setTexture(PARTICLE_PNG, PARTICLE_PNG_LENGTH);
