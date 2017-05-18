9458817c5294219eda854c45fd6d8196521c1f1d
Albin Bernhardsson <catscratchesp@gmail.com>
2017-05-18 15:17:51 +0200

    Use device local memory for storage buffers

--- src/StorageBuffer.cpp
+++ src/StorageBuffer.cpp
@@ -23,7 +23,7 @@ StorageBuffer::StorageBuffer(const void* data, unsigned int size, VkDevice devic
     vkUnmapMemory(device, stagingBufferMemory);
 
     // Actual buffer.
-    createBuffer(size, VK_BUFFER_USAGE_STORAGE_BUFFER_BIT | VK_BUFFER_USAGE_TRANSFER_DST_BIT, VK_MEMORY_PROPERTY_HOST_COHERENT_BIT, &buffer, &bufferMemory);
+    createBuffer(size, VK_BUFFER_USAGE_STORAGE_BUFFER_BIT | VK_BUFFER_USAGE_TRANSFER_DST_BIT, VK_MEMORY_PROPERTY_DEVICE_LOCAL_BIT, &buffer, &bufferMemory);
     
     // Copy from staging buffer to actual buffer.
     copyBuffer(stagingBuffer, buffer, size);
